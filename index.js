f715127f2bb6e8883128964e3d3ef3cea7a9e9c3
Kluddizz <flhansen.93@gmail.com>
2020-10-03 00:06:37 +0200

    Initial commit. Add github route.

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Install middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(helmet());
+app.use(cors());
+
+// The following code is used to load all routes inside the routes folder
+// automatically. Every route is represented by a 'express.Router' instance.
+const routesPath = path.join(__dirname, "routes");
+
+fs.readdirSync(routesPath).forEach((filename) => {
+  const routePath = `./routes/${filename}`;
+
+  if (path.extname(routePath) == ".js") {
+    const routeName = path.basename(filename, ".js");
+    const route = require(routePath);
+    app.use(`/${routeName}`, route);
+  }
+});
+
+// Start the backend server.
+app.listen(port, () => {
+  console.log(`webserver running on port ${port}`);
+});
